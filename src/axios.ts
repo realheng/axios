cebc27ce92e3c010831935e183721e210e5828c4
refrain <yuzhenheng@landray.com.cn>
2020-12-06 22:50:43 +0800

    扩展axios.create静态接口

--- src/axios.ts
+++ src/axios.ts
@@ -1,18 +1,22 @@
-import { AxiosInstance, AxiosRequestConfig } from './types/index'
+import { AxiosInstance, AxiosRequestConfig, AxiosStatic } from './types/index'
 import Axios from './core/Axios'
 import { extend } from './utils/common'
 import defaults from './defaults'
 
 // 创建一个拥有一系列请求方法的函数
-function createInstance(config: AxiosRequestConfig): AxiosInstance {
+function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(config)
   const instance = Axios.prototype.request.bind(context)
 
   extend(instance, context)
 
-  return instance as AxiosInstance
+  return instance as AxiosStatic
 }
 
 const axios = createInstance(defaults)
+
+axios.create = function create(config: AxiosRequestConfig) {
+  return createInstance(config)
+}
 
 export default axios
